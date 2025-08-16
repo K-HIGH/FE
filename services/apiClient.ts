d4e745326228042f07f274afcee2b63a7440e624
김주현 <juhy0987@naver.com>
2025-08-16 12:19:29 +0900

    [TODO]: 세션 캐싱 전략 필요 추가

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -7,6 +7,7 @@ const api = axios.create({
   withCredentials: true, // 서버에서 httpOnly 쿠키 세션(sid)을 내려줄 경우 필요
 });
 
+// TODO: 세션 캐싱 전략 필요
 api.interceptors.request.use(async (config) => {
     const { data: { session } } = await supabase.auth.getSession();
     if (session?.access_token) {
